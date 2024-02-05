e85d793b7dc65158990489578f059f0b5462205c
Crowdbotics <team@crowdbotics.com>
2024-02-05 16:54:35 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "education-doc-stora-45927",
+  url: "https://education-doc-stora-45927.botics.co",
+  api: "https://education-doc-stora-45927.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
